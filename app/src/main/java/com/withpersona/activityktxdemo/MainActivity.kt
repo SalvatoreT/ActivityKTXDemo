e3aaf9827e694196a9588e0de86aa81b48999c32
Salvatore Testa <sal@withpersona.com>
2020-07-31 00:49:26 -0700

    Access fine location with alpha06

--- app/src/main/java/com/withpersona/activityktxdemo/MainActivity.kt
+++ app/src/main/java/com/withpersona/activityktxdemo/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.withpersona.activityktxdemo
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+  private val permissionLauncher =
+    registerForActivityResult(ActivityResultContracts.RequestPermission()) { success ->
+    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(findViewById(R.id.toolbar))
 
-    findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null)
-          .show()
+    findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+      permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
     }
   }
 
